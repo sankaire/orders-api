7580e9bbc96aad17979dd66875336abb5539d407
sankaire <kerenketepela@gmail.com>
2024-02-20 18:08:31 +0300

    Feat:Login customer handler

--- internal/handlers/index.go
+++ internal/handlers/index.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/sankaire/orders-api/internal/db/internal/repository"
+	"github.com/sankaire/orders-api/internal/db/internal/utils"
 	"net/http"
 )
 
@@ -15,8 +16,10 @@ func CreateCustomerHandler(response http.ResponseWriter, request *http.Request) 
 	}
 
 	var customer struct {
-		Name string `json:"name"`
-		Code string `json:"code"`
+		Name     string `json:"name"`
+		Email    string `json:"email"`
+		Phone    string `json:"phone"`
+		Password string `json:"password"`
 	}
 
 	if err := json.NewDecoder(request.Body).Decode(&customer); err != nil {
@@ -24,28 +27,35 @@ func CreateCustomerHandler(response http.ResponseWriter, request *http.Request) 
 		fmt.Fprintf(response, "Invalid request body")
 		return
 	}
-
-	customerID, err := repository.CreateCustomer(customer.Name, customer.Code)
+	hashedPassword, err := utils.EncryptPassword(customer.Password)
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(response, "An error occured")
+		return
+	}
+	customerID, err := repository.CreateCustomer(customer.Name, customer.Phone, customer.Email, hashedPassword)
 	if err != nil {
 		response.WriteHeader(http.StatusInternalServerError)
 		fmt.Fprintf(response, "Error creating customer: %v", err)
 		return
 	}
 
-	name, code, err := repository.ReadCustomer(customerID)
+	name, email, phone, err := repository.ReadCustomer(customerID)
 	if err != nil {
 		response.WriteHeader(http.StatusInternalServerError)
 		fmt.Fprintf(response, "Error reading customer: %v", err)
 		return
 	}
 	createdCustomer := struct {
-		ID   int64  `json:"id"`
-		Name string `json:"name"`
-		Code string `json:"code"`
+		ID    int64  `json:"id"`
+		Name  string `json:"name"`
+		Phone string `json:"phone"`
+		Email string `json:"email"`
 	}{
-		ID:   customerID,
-		Name: name,
-		Code: code,
+		ID:    customerID,
+		Name:  name,
+		Phone: phone,
+		Email: email,
 	}
 	jsonResponse, err := json.Marshal(createdCustomer)
 	if err != nil {
@@ -75,6 +85,51 @@ func ReadCustomersHandler(response http.ResponseWriter, request *http.Request) {
 		fmt.Fprintf(response, "Error marshaling JSON: %v", err)
 		return
 	}
+	response.Header().Set("Content-Type", "application/json")
+	response.WriteHeader(http.StatusCreated)
+	response.Write(jsonResponse)
+}
+
+func LoginCustomerHandler(response http.ResponseWriter, request *http.Request) {
+	if request.Method != http.MethodPost {
+		response.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprintf(response, "Method not allowed")
+		return
+	}
+	var creds struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+	err := json.NewDecoder(request.Body).Decode(&creds)
+	if err != nil {
+		http.Error(response, err.Error(), http.StatusBadRequest)
+		return
+	}
+	id, email, phone, password, err := repository.ReadCustomerByEmail(creds.Email)
+	if err != nil {
+		http.Error(response, err.Error(), http.StatusBadRequest)
+		return
+	}
+	_, err = utils.ComparePassword(password, creds.Password)
+	if err != nil {
+		http.Error(response, err.Error(), http.StatusBadRequest)
+		return
+	}
+	Payload := struct {
+		ID    int    `json:"ID"`
+		Email string `json:"email"`
+		Phone string `json:"phone"`
+	}{
+		ID:    id,
+		Email: email,
+		Phone: phone,
+	}
+	accessToken, err := utils.CreateToken(Payload)
+	if err != nil {
+		http.Error(response, err.Error(), http.StatusBadRequest)
+		return
+	}
+	jsonResponse, err := json.Marshal(accessToken)
 	response.Header().Set("Content-Type", "application/json")
 	response.WriteHeader(http.StatusCreated)
 	response.Write(jsonResponse)
