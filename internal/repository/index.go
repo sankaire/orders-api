eb11f74261d5f9eaaed11604a7b1b70c5c86de79
sankaire <kerenketepela@gmail.com>
2024-02-20 18:08:58 +0300

    Feat:read customer by email

--- internal/repository/index.go
+++ internal/repository/index.go
@@ -7,19 +7,21 @@ import (
 )
 
 type Customers struct {
-	ID   int
-	Name string
-	Code string
+	ID       int
+	Name     string
+	Phone    string
+	Email    string
+	Password string
 }
 
-func CreateCustomer(name string, code string) (int64, error) {
+func CreateCustomer(name string, phone string, email string, password string) (int64, error) {
 	schema, err := db.Connect()
 	if err != nil {
 		return 0, err
 	}
 	defer schema.Close()
 	var customerID int64
-	err = schema.QueryRow("INSERT INTO customers (name, code) VALUES ($1, $2) RETURNING id", name, code).Scan(&customerID)
+	err = schema.QueryRow("INSERT INTO customers (name, phone, email, password) VALUES ($1, $2,$3,$4 ) RETURNING id", name, phone, email, password).Scan(&customerID)
 	if err != nil {
 		panic(err)
 	}
@@ -34,7 +36,7 @@ func ReadCustomers() ([]Customers, error) {
 		return []Customers{}, err
 	}
 	defer schema.Close()
-	rows, err := schema.Query("SELECT id, name, code FROM customers")
+	rows, err := schema.Query("SELECT id, name, email, phone,password FROM customers")
 	if err != nil {
 		return nil, err
 	}
@@ -43,7 +45,7 @@ func ReadCustomers() ([]Customers, error) {
 	var customers []Customers
 	for rows.Next() {
 		var customer Customers
-		if err := rows.Scan(&customer.ID, &customer.Name, &customer.Code); err != nil {
+		if err := rows.Scan(&customer.ID, &customer.Name, &customer.Email, &customer.Phone, &customer.Password); err != nil {
 			return nil, err
 		}
 		customers = append(customers, customer)
@@ -54,20 +56,38 @@ func ReadCustomers() ([]Customers, error) {
 
 	return customers, nil
 }
-func ReadCustomer(customerID int64) (string, string, error) {
+func ReadCustomer(customerID int64) (string, string, string, error) {
 	schema, err := db.Connect()
 	if err != nil {
-		return "", "", err
+		return "", "", "", err
 	}
 	defer schema.Close()
-	var name, code string
-	err = schema.QueryRow("SELECT name, code FROM customers WHERE id = $1", customerID).Scan(&name, &code)
+	var name, phone, email string
+	err = schema.QueryRow("SELECT name, phone, email FROM customers WHERE id = $1", customerID).Scan(&name, &phone, &email)
 	if err != nil {
 		if err == sql.ErrNoRows {
-			return "", "", fmt.Errorf("customer with ID %d not found", customerID)
+			return "", "", "", fmt.Errorf("customer with ID %d not found", customerID)
 		}
-		return "", "", err
+		return "", "", "", err
 	}
 
-	return name, code, nil
+	return name, email, phone, nil
+}
+func ReadCustomerByEmail(email string) (int, string, string, string, error) {
+	schema, err := db.Connect()
+	if err != nil {
+		return 0, "", "", "", nil
+	}
+	defer schema.Close()
+
+	var customer Customers
+	err = schema.QueryRow("SELECT id,phone,email,password FROM customers WHERE email = $1", email).Scan(&customer.ID, &customer.Phone, &customer.Email, &customer.Password)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return 0, "", "", "", fmt.Errorf("customer with email %s not found", email)
+		}
+		return 0, "", "", "", nil
+	}
+
+	return customer.ID, customer.Email, customer.Phone, customer.Password, nil
 }
