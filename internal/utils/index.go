4b16fc2d7d285e7aa3f62c3b73c05bc1a8457046
sankaire <kerenketepela@gmail.com>
2024-02-21 12:40:57 +0300

    Feat:send sms intergration - AT

--- internal/utils/index.go
+++ internal/utils/index.go
@@ -1,9 +1,15 @@
 package utils
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"github.com/golang-jwt/jwt/v5"
+	"github.com/joho/godotenv"
 	"golang.org/x/crypto/bcrypt"
+	"net/http"
+	url2 "net/url"
+	"os"
 	"time"
 )
 
@@ -16,6 +22,11 @@ type JWTPayload struct {
 	ID    int    `json:"ID"`
 	Email string `json:"email"`
 	Phone string `json:"phone"`
+}
+type ResponseEnvelope struct {
+	Success bool        `json:"success"`
+	Message string      `json:"message,omitempty"`
+	Data    interface{} `json:"data,omitempty"`
 }
 
 var secretKey = []byte("secret-key")
@@ -48,7 +59,7 @@ func CreateToken(payload JWTPayload) (string, error) {
 	}
 	return tokenString, err
 }
-func verifyToken(tokenString string) (*jwt.Token, error) {
+func VerifyToken(tokenString string) (*jwt.Token, error) {
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		return secretKey, nil
 	})
@@ -59,4 +70,57 @@ func verifyToken(tokenString string) (*jwt.Token, error) {
 		return nil, fmt.Errorf("invalid token")
 	}
 	return token, nil
+}
+
+func WriteResponse(w http.ResponseWriter, statusCode int, success bool, message string, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+
+	envelope := ResponseEnvelope{
+		Success: success,
+		Message: message,
+		Data:    data,
+	}
+	json.NewEncoder(w).Encode(envelope)
+}
+func SendSms(phone string, item string, amount int64) error {
+	url := "https://api.africastalking.com/version1/messaging"
+	err := godotenv.Load()
+	//if err != nil {
+	//	log.Fatal("Error loading .env file")
+	//
+	//}
+	apiKey := os.Getenv("AT_API_KEY")
+	userName := os.Getenv("AT_USERNAME")
+
+	params := url2.Values{
+		"username": {userName},
+		"to":       {phone},
+		"message":  {fmt.Sprintf("Your order of %s has been placed successfully. You will pay KES %d on delivery.", item, amount)},
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer([]byte(params.Encode())))
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return err
+	}
+
+	setHeaders(req, apiKey)
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error sending request:", err)
+		return err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusCreated {
+		fmt.Println("Unexpected response status:", resp.Status)
+		return err
+	}
+	return nil
+}
+func setHeaders(request *http.Request, apiKey string) {
+	request.Header.Set("apiKey", apiKey)
+	request.Header.Set("Accept", "application/json")
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 }
