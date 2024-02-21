ca360811f1aac56dd850b6f131a07af938348ea4
sankaire <kerenketepela@gmail.com>
2024-02-21 12:39:03 +0300

    Feat:register orders API, protect routes

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/sankaire/orders-api/internal/db/internal/db"
 	"github.com/sankaire/orders-api/internal/db/internal/handlers"
+	"github.com/sankaire/orders-api/internal/db/internal/middleware"
 	"log"
 	"net/http"
 )
@@ -11,8 +12,10 @@ import (
 func main() {
 	db.CreateTables()
 	http.HandleFunc("/api/customer", handlers.CreateCustomerHandler)
-	http.HandleFunc("/api/customers", handlers.ReadCustomersHandler)
+	http.Handle("/api/customers", middleware.Authenticate(http.HandlerFunc(handlers.ReadCustomersHandler)))
 	http.HandleFunc("/api/customer/login", handlers.LoginCustomerHandler)
+	http.Handle("/api/order", middleware.Authenticate(http.HandlerFunc(handlers.CreateCustomerOrder)))
+	http.Handle("/api/orders", middleware.Authenticate(http.HandlerFunc(handlers.ReadAllCustomerOrdersHandler)))
 	fmt.Println("Server is running on port 8080...")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
