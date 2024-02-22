6e409a6a87cd0726cde0c088fb856f9dfd00ae88
sankaire <kerenketepela@gmail.com>
2024-02-22 09:56:54 +0300

    Chore:get port from env

--- cmd/main.go
+++ cmd/main.go
@@ -2,20 +2,27 @@ package main
 
 import (
 	"fmt"
+	"github.com/joho/godotenv"
 	"github.com/sankaire/orders-api/internal/db/internal/db"
 	"github.com/sankaire/orders-api/internal/db/internal/handlers"
 	"github.com/sankaire/orders-api/internal/db/internal/middleware"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	db.CreateTables()
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	port := os.Getenv("PORT")
 	http.HandleFunc("/api/customer", handlers.CreateCustomerHandler)
 	http.Handle("/api/customers", middleware.Authenticate(http.HandlerFunc(handlers.ReadCustomersHandler)))
 	http.HandleFunc("/api/customer/login", handlers.LoginCustomerHandler)
 	http.Handle("/api/order", middleware.Authenticate(http.HandlerFunc(handlers.CreateCustomerOrder)))
 	http.Handle("/api/orders", middleware.Authenticate(http.HandlerFunc(handlers.ReadAllCustomerOrdersHandler)))
-	fmt.Println("Server is running on port 8080...")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	fmt.Printf("Server is running on port %s\n", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
